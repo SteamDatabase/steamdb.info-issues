26efc1cb495e65a36c06dda25edcb0555170031a
Pavel <me@xpaw.ru>
2013-02-16 10:09:26 -0800

    Add userscript

---
+++ SteamStoreIntegration.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @version        1.0
+// @name           Steam Store - Apps Database Link
+// @namespace      http://cdr.xpaw.ru/?user=store
+// @match          http://store.steampowered.com/app/*
+// @match          http://store.steampowered.com/sub/*
+// ==/UserScript==
+
+var mainURL = 'http://cdr.xpaw.ru',
+	appid = location.pathname.match( /(\d)+/g ),
+	isSubPage = location.pathname.match( /\/sub\// ),
+	demoBlock = document.querySelector( isSubPage ? '.share' : '.demo_area_button' );
+
+if( !demoBlock )
+{
+	var demoBlock = document.querySelector( '.error' );
+	
+	if( demoBlock )
+	{
+		demoBlock.innerHTML += '<br><br><a href="'+ mainURL + '/app/' + appid + '/" target="_blank">View in Apps Database</a>';
+	}
+	
+	return;
+}
+
+var element = document.createElement( 'div' );
+element.className = 'demo_area_button';
+element.innerHTML = '<a class="game_area_wishlist_btn" href="' + mainURL + ( isSubPage ? '/sub/' : '/app/' ) + appid + '/" target="_blank">View in Apps Database</a>';
+
+demoBlock.parentNode.insertBefore( element, demoBlock );
