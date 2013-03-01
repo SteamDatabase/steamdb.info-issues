160309ac23e132d72103c9a0537cc1e5fc567002
Pavel <me@xpaw.ru>
2013-03-01 17:13:27 +0200

    Integrate to steam community (hubs/game groups)
    Fixes #17

--- SteamStoreIntegration.user.js
+++ SteamStoreIntegration.user.js
@@ -1,30 +1,88 @@
 // ==UserScript==
-// @version        1.0
-// @name           Steam Store - Apps Database Link
-// @namespace      http://steamdb.info/?user=store
+// @version        1.1
+// @name           Steam Apps Database Integration
+// @description    Adds Steam Database link across Steam Community and Store
+// @namespace      http://steamdb.info/userscript/
 // @match          http://store.steampowered.com/app/*
 // @match          http://store.steampowered.com/sub/*
+// @match          http://steamcommunity.com/app/*
+// @match          http://steamcommunity.com/games/*
 // ==/UserScript==
 
 var mainURL = 'http://steamdb.info',
 	appid = location.pathname.match( /(\d)+/g ),
-	isSubPage = location.pathname.match( /\/sub\// ),
-	demoBlock = document.querySelector( isSubPage ? '.share' : '.demo_area_button' );
+	element, container;
 
-if( !demoBlock )
+if( location.hostname === 'steamcommunity.com' )
 {
-	var demoBlock = document.querySelector( '.error' );
-	
-	if( demoBlock )
+	if( location.pathname.match( /\/app\// ) )
 	{
-		demoBlock.innerHTML += '<br><br><a href="'+ mainURL + '/app/' + appid + '/" target="_blank">View in Apps Database</a>';
+		// Game Hub
+		
+		container = document.querySelector( '.apphub_OtherSiteInfo' );
+		
+		if( !container )
+		{
+			return;
+		}
+		
+		element = document.createElement( 'a' );
+		element.className = 'btn_green_white_innerfade btn_medium';
+		element.href = mainURL + '/app/' + appid + '/';
+		element.target = '_blank';
+		element.innerHTML = '<span>Steam Database</span>';
+		
+		container.insertBefore( element, container.firstChild );
+	}
+	else
+	{
+		// Game Group
+		
+		container = document.querySelector( '#rightActionBlock' );
+		
+		if( !container )
+		{
+			return;
+		}
+		
+		element = document.createElement( 'div' );
+		element.className = 'actionItem';
+		element.innerHTML = '<div class="actionItemIcon"></div><a class="linkActionMinor" target="_blank" href="' + mainURL + '/app/' + appid + '/">View in Steam Database</a>';
+		
+		//container.insertBefore( element, container.firstChild );
+		container.insertBefore( element, null );
+		
+		// While we're here, let's fix this annoyance
+		element = document.querySelector( '#oggLogo img' );
+		
+		if( element )
+		{
+			element.style.height = '208px';
+		}
+	}
+}
+else
+{
+	var isSubPage = location.pathname.match( /\/sub\// );
+	
+	container = document.querySelector( isSubPage ? '.share' : '#demo_block .block_content_inner' );
+	
+	// Did we hit an error page?
+	if( !container )
+	{
+		container = document.querySelector( '.error' );
+		
+		if( container )
+		{
+			container.innerHTML += '<br><br><a target="_blank" href="'+ mainURL + '/app/' + appid + '/">View in Steam Database</a>';
+		}
+		
+		return;
 	}
 	
-	return;
+	element = document.createElement( 'div' );
+	element.className = 'demo_area_button';
+	element.innerHTML = '<a class="game_area_wishlist_btn" target="_blank" href="' + mainURL + ( isSubPage ? '/sub/' : '/app/' ) + appid + '/">View in Steam Database</a>';
+
+	container.insertBefore( element, container.firstChild );
 }
-
-var element = document.createElement( 'div' );
-element.className = 'demo_area_button';
-element.innerHTML = '<a class="game_area_wishlist_btn" href="' + mainURL + ( isSubPage ? '/sub/' : '/app/' ) + appid + '/" target="_blank">View in Apps Database</a>';
-
-demoBlock.parentNode.insertBefore( element, demoBlock );
